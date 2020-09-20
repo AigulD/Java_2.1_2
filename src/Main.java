10b30f33008255fdc1bbb1e5e3d4cc568f994680
AigulD <70228729+AigulD@users.noreply.github.com>
2020-09-20 21:18:34 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ public class Main {
     public static void main(String[] args) {
         long initialBalance = 100;
         long replenishmentAmount = 3000;
-        long totalBonus;
+        long bonus;
         if (replenishmentAmount >= 1000) {
-            totalBonus = replenishmentAmount / 100;
+            bonus = replenishmentAmount / 100;
         } else {
-            totalBonus = 0;
+            bonus = 0;
         }
-               System.out.println(totalBonus);
+        long totalBalance = initialBalance + replenishmentAmount + bonus;
+        System.out.println(bonus);
+        System.out.println(totalBalance);
     }
 }
